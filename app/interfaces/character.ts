a2e3207c3fa5835336d18faa30de3653bcc5233e
Santiago Fernandez <fernandezsantid@gmail.com>
2022-09-14 15:45:46 -0300

    Change api file extension. Changes in Character interface

--- app/interfaces/character.ts
+++ app/interfaces/character.ts
@@ -1,14 +1,14 @@
 export interface Character {
-  id: number;
-  name: string;
-  status: 'Alive' | 'Dead' | 'unknown';
-  species: string;
-  type: string;
-  gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
-  origin: {name: string; url: string};
-  location: {name: string; url: string};
-  image: string;
-  episode: string[];
-  url: string;
-  created: string;
+  id: any;
+  name: any;
+  status: any;
+  species: any;
+  type: any;
+  gender: any;
+  origin: any;
+  location: any;
+  image: any;
+  episode: any;
+  url: any;
+  created: any;
 }
